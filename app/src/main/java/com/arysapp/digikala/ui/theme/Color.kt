b9baf5237db02028be73e3908c9cf9d4b9ecfe8b
safaryaryan <safaryaryan@gmail.com>
2024-11-02 22:06:07 +0330

    Customize Theme

--- app/src/main/java/com/arysapp/digikala/ui/theme/Color.kt
+++ app/src/main/java/com/arysapp/digikala/ui/theme/Color.kt
@@ -1,11 +1,44 @@
 package com.arysapp.digikala.ui.theme
 
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 
+enum class  CustomTheme{
+    LIGHT , DARK
+}
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val red = Color(0xFFEF3F3E)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+@Stable
+class CustomColor(
+    backgroundColor: Color,
+    textColor: Color,
+    mainColor: Color
+){
+   var backgroundColor by mutableStateOf(backgroundColor)
+    private set
+
+    var textColor by mutableStateOf(textColor)
+        private set
+
+    var mainColor by mutableStateOf(mainColor)
+        private set
+
+    fun updateColor(colors: CustomColor){
+      this.backgroundColor = colors.backgroundColor
+      this.textColor = colors.textColor
+      this.mainColor = colors.mainColor
+    }
+
+    fun initColor()= CustomColor(backgroundColor, textColor, mainColor)
+}
--- app/src/main/java/com/arysapp/digikala/ui/theme/Theme.kt
+++ app/src/main/java/com/arysapp/digikala/ui/theme/Theme.kt
@@ -1,58 +1,74 @@
 package com.arysapp.digikala.ui.theme
-
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
+private val CustomDarkTheme = CustomColor(
+    backgroundColor = black ,
+    textColor = white ,
+    mainColor = red
 )
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val CustomLightTheme = CustomColor(
+    backgroundColor = white ,
+    textColor = black ,
+    mainColor = red
 )
+private val LocalColorsProvider = staticCompositionLocalOf {
+    CustomLightTheme
+}
+
+@Composable
+private fun CustomLocalProvider(
+    colors: CustomColor,
+    content:@Composable () -> Unit
+) {
+    val colorPalette  = remember {
+        colors.initColor()
+    }
+    colorPalette.updateColor(colors)
+    CompositionLocalProvider(
+        value = LocalColorsProvider provides colorPalette,
+        content = content
+        )
+}
+
+private val CustomTheme.colors : Pair<ColorScheme, CustomColor>
+    get() = when(this){
+      CustomTheme.DARK -> darkColorScheme()   to CustomDarkTheme
+      CustomTheme.LIGHT -> lightColorScheme() to CustomLightTheme
+    }
+
+object CustomThemeManager{
+ val colors : CustomColor
+    @Composable
+    get() = LocalColorsProvider.current
+    val customTheme by mutableStateOf(CustomTheme.LIGHT)
+    fun isSystemInDarkTheme():Boolean{
+    return customTheme== CustomTheme.DARK
+    }
+
+
+}
 
 @Composable
 fun DigiKalaTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    customTheme: CustomTheme = CustomThemeManager.customTheme,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val(colorPalette , myColor)=customTheme.colors
+    CustomLocalProvider(colors = myColor) {
+        MaterialTheme(
+            colorScheme = colorPalette,
+            typography = Typography,
+            content = content
+        )
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+
 }
