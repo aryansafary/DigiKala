2f5f0d5242080c131fb486fec88969762c04fe17
safaryaryan <safaryaryan@gmail.com>
2024-11-03 18:47:03 +0330

    completed SplashScreen

--- app/src/main/java/com/arysapp/digikala/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/arysapp/digikala/ui/screens/SplashScreen.kt
@@ -1,9 +1,64 @@
 package com.arysapp.digikala.ui.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.arysapp.digikala.R
+import com.arysapp.digikala.navigation.Screens
+import com.arysapp.digikala.ui.componets.Loading3Dots
+import com.arysapp.digikala.ui.theme.CustomThemeManager
+import kotlinx.coroutines.delay
+
 @Composable
 fun SplashScreen(navController: NavController){
-    Text(text = " SplashScreen ")
+Splash()
+LaunchedEffect(true) {
+    delay(3500)
+    navController.navigate(Screens.HomeScreen.route)
+}
+}
+
+@Composable
+fun Splash(){
+    Box(modifier =
+    Modifier
+        .fillMaxSize()
+        .background(CustomThemeManager.colors.mainColor),
+        contentAlignment = Alignment.Center
+    ){
+Image(
+    modifier = Modifier.size(250.dp),
+    painter = painterResource(id = R.drawable.digikala_logo) ,
+    contentDescription ="Logo" )
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(100.dp),
+       contentAlignment = Alignment.BottomCenter
+        ){
+        Image(
+            modifier = Modifier.height(30.dp),
+            painter = painterResource(id = R.drawable.digikala_txt_white) ,
+            contentDescription ="TextLogo" )
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        contentAlignment = Alignment.BottomCenter
+    ){
+Loading3Dots(isDark = false)
+    }
 }
--- app/src/main/java/com/arysapp/digikala/ui/theme/Color.kt
+++ app/src/main/java/com/arysapp/digikala/ui/theme/Color.kt
@@ -16,7 +16,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val red = Color(0xFFEF3F3E)
+val red = Color(0xFFED1B34)
 val black = Color(0xFF000000)
 val white = Color(0xFFFFFFFF)
 @Stable
